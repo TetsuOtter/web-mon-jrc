55a2f4b546a9b3f026322b5b6189882a13a8c269
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2026-02-11 22:27:48 +0900

    種別用のTypeCellを追加

---
+++ dev/app/src/pages/monitors/type313s/pages/work-setting/components/TypeCell.tsx
@@ -0,0 +1,71 @@
+import { memo } from "react";
+
+import { CanvasText } from "../../../../../../canvas-renderer";
+import { useCanvasObjectContext } from "../../../../../../canvas-renderer/contexts/CanvasObjectContext";
+import CanvasObjectGroup from "../../../../../../canvas-renderer/objects/CanvasObjectGroup";
+import { COLORS, FONT_SIZE_2X } from "../../../constants";
+
+import type { CellProps } from "./SelectionGrid";
+
+const FIRST_LINE_X = 32;
+const FIRST_LINE_Y = 4;
+const SECOND_LINE_X = FIRST_LINE_X;
+const SECOND_LINE_Y = FIRST_LINE_Y + FONT_SIZE_2X + 4;
+
+export default memo<CellProps>(function TypeCell({ index }) {
+	const {
+		metadata: { height, width },
+	} = useCanvasObjectContext();
+	if (ELEM_LIST.length <= index) {
+		return null;
+	}
+	const typeName = ELEM_LIST[index];
+	const firstLine = typeName.split("\n")[0];
+	const secondLine = typeName.split("\n")[1];
+	if (!secondLine) {
+		return (
+			<CanvasText
+				relX={FIRST_LINE_X}
+				relY={FIRST_LINE_Y}
+				text={firstLine}
+				scaleY={2}
+				fillColor={COLORS.WHITE}
+			/>
+		);
+	}
+	return (
+		<CanvasObjectGroup
+			relX={0}
+			relY={0}
+			width={width}
+			height={height}>
+			<CanvasText
+				relX={FIRST_LINE_X}
+				relY={FIRST_LINE_Y}
+				text={firstLine}
+				scaleY={2}
+				fillColor={COLORS.WHITE}
+			/>
+			<CanvasText
+				relX={SECOND_LINE_X}
+				relY={SECOND_LINE_Y}
+				text={secondLine}
+				fillColor={COLORS.WHITE}
+			/>
+		</CanvasObjectGroup>
+	);
+});
+
+export const ELEM_LIST: string[] = [
+	"回　送",
+	"試運転",
+	"臨　時",
+	"団　体",
+	"普　通",
+	"快　速",
+	"ワンマン",
+	"ワンマン普通",
+	"ワンマン快速",
+	"通勤快速",
+	"普　通\n（熱海）",
+];
