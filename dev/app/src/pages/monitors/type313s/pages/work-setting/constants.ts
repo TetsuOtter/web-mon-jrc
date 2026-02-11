73ead1cc24d69b003ac063f2689847aabc49235a
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2026-02-11 22:49:38 +0900

    不要な設定を削除

--- dev/app/src/pages/monitors/type313s/pages/work-setting/constants.ts
+++ dev/app/src/pages/monitors/type313s/pages/work-setting/constants.ts
@@ -1,6 +1,5 @@
-import { PAGE_MODES, PAGE_TYPES } from "../pageTypes";
+import { PAGE_MODES } from "../pageTypes";
 
-import type { FooterButtonInfo } from "../../footer/FooterArea";
 import type { useWorkSettingPageMode } from "../../hooks/usePageMode";
 
 export const PAGE_NAME_MAP = {
@@ -8,20 +7,3 @@ export const PAGE_NAME_MAP = {
 	[PAGE_MODES.DRIVER]: "運転士運行設定",
 	[PAGE_MODES.CONDUCTOR]: "車掌運行設定",
 } as const satisfies Record<ReturnType<typeof useWorkSettingPageMode>, string>;
-
-export const FOOTER_MENU = [
-	{
-		label: "運行タイプ",
-		navigateTo: PAGE_TYPES.WORK_SETTING_TYPE,
-		queryParams: { mode: "WORK_SETTING" },
-	},
-	{
-		label: "運転席",
-		navigateTo: PAGE_TYPES.WORK_SETTING_SEAT,
-		queryParams: { mode: "WORK_SETTING" },
-	},
-	{
-		label: "戻る",
-		useBackNavigation: true,
-	},
-] as const satisfies FooterButtonInfo[];
