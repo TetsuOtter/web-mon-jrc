f60b114f8ae225c0a79fed20fc79231cd497643c
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2026-02-24 01:38:07 +0900

    地点補正ページ実装

--- dev/app/src/pages/monitors/type313s/pages/correction/LocationCorrection.tsx
+++ dev/app/src/pages/monitors/type313s/pages/correction/LocationCorrection.tsx
@@ -1,27 +1,108 @@
-import { memo } from "react";
+import { memo, useCallback, useState } from "react";
 
-import { CanvasText } from "../../../../../canvas-renderer";
+import { CanvasRect, CanvasText } from "../../../../../canvas-renderer";
+import Button, { SHADOW_WIDTH } from "../../components/Button";
 import FooterPageFrame from "../../components/FooterPageFrame";
+import LocationLabel from "../../components/LocationLabel";
+import TrainFormationImage from "../../components/car-image/TrainFormationImage";
 import { COLORS } from "../../constants";
+import { useFooterAreaWithPagerProps } from "../../footer/FooterAreaWithPagerPropsHook";
 import { useCorrectionPageMode } from "../../hooks/usePageMode";
+import { DOOR_STATE_GRID_ROW_DEFINITION } from "../conductor/ConductorGridRowDefinitions";
+import ConductorStateGrid from "../conductor/components/ConductorStateGrid";
 import { PAGE_TYPES } from "../pageTypes";
 
+import LocationCorrectionLineImage, {
+	CELL_COUNT as LINE_IMAGE_CELL_COUNT,
+} from "./components/LocationCorrectionLineImage";
+
 import type { FooterButtonInfo } from "../../footer/FooterArea";
+import type { GridRowDefinition } from "../conductor/components/ConductorStateGrid";
+
+const CELL_PER_PAGE = LINE_IMAGE_CELL_COUNT * 2;
+
+const CONFIRM_RECT_X = 200;
+const CONFIRM_RECT_Y = 416;
+const CONFIRM_RECT_WIDTH = 432;
+const CONFIRM_RECT_HEIGHT = 64;
+
+const LABEL_X = 16;
+
+const CONFIRM_BUTTON_X = 320;
+const CONFIRM_BUTTON_Y = 16;
+const CONFIRM_BUTTON_WIDTH = 96;
+const CONFIRM_BUTTON_HEIGHT = 32;
 
 export default memo(function DriverLocationCorrection() {
 	const mode = useCorrectionPageMode();
+	const [carImageStationIndex, setCarImageStationIndex] = useState<number>(0);
+	const pagerProps = useFooterAreaWithPagerProps(
+		Math.floor(STA_NAME_LIST.length / CELL_PER_PAGE)
+	);
+	const onClickConfirm = useCallback(() => {
+		console.log("確認", {
+			carImageStationIndex,
+		});
+	}, [carImageStationIndex]);
 	return (
 		<FooterPageFrame
 			mode={mode}
-			footerItems={FOOTER_MENU}>
-			<CanvasText
-				relX={0}
-				relY={0}
-				verticalAlign="center"
-				align="center"
-				text="準備中"
-				fillColor={COLORS.WHITE}
+			footerItems={FOOTER_MENU}
+			pagerProps={pagerProps}>
+			<LocationLabel />
+			<TrainFormationImage />
+			<ConductorStateGrid rowDefinitionList={GRID_DEFINITION} />
+
+			<LocationCorrectionLineImage
+				row={0}
+				carImageStationIndex={carImageStationIndex}
+				onClick={setCarImageStationIndex}
+				stationNameStartIndex={pagerProps.currentPageIndex * CELL_PER_PAGE}
+				stationNameList={STA_NAME_LIST}
 			/>
+			<LocationCorrectionLineImage
+				row={1}
+				carImageStationIndex={carImageStationIndex}
+				onClick={setCarImageStationIndex}
+				stationNameStartIndex={
+					pagerProps.currentPageIndex * CELL_PER_PAGE + LINE_IMAGE_CELL_COUNT
+				}
+				stationNameList={STA_NAME_LIST}
+			/>
+
+			<CanvasRect
+				relX={CONFIRM_RECT_X}
+				relY={CONFIRM_RECT_Y}
+				width={CONFIRM_RECT_WIDTH}
+				height={CONFIRM_RECT_HEIGHT}
+				strokeColor={COLORS.WHITE}
+				strokeWidth={1}>
+				<CanvasText
+					text={
+						"駅名にタッチして下さい。\nよろしければ「確認」を押して下さい。"
+					}
+					fillColor={COLORS.WHITE}
+					relX={LABEL_X}
+					relY={0}
+					verticalAlign="center"
+				/>
+				<Button
+					relX={CONFIRM_BUTTON_X}
+					relY={CONFIRM_BUTTON_Y}
+					width={CONFIRM_BUTTON_WIDTH}
+					height={CONFIRM_BUTTON_HEIGHT}
+					onClick={onClickConfirm}
+					shadowWidth={SHADOW_WIDTH.SMALL}>
+					<CanvasText
+						text="確　認"
+						fillColor={COLORS.WHITE}
+						relX={0}
+						relY={0}
+						align="center"
+						verticalAlign="center"
+					/>
+				</Button>
+			</CanvasRect>
 		</FooterPageFrame>
 	);
 });
@@ -36,3 +117,40 @@ const FOOTER_MENU = [
 		useBackNavigation: true,
 	},
 ] as const satisfies FooterButtonInfo[];
+
+const GRID_DEFINITION = [
+	DOOR_STATE_GRID_ROW_DEFINITION,
+] as const satisfies GridRowDefinition[];
+
+const STA_NAME_LIST = [
+	"東京",
+	"神田",
+	"秋葉原",
+	"御徒町",
+	"上野",
+	"鶯谷",
+	"日暮里",
+	"西日暮里",
+	"田端",
+	"駒込",
+	"巣鴨",
+	"大塚",
+	"池袋",
+	"目白",
+	"高田馬場",
+	"新大久保",
+	"新宿",
+	"代々木",
+	"原宿",
+	"渋谷",
+	"恵比寿",
+	"目黒",
+	"五反田",
+	"大崎",
+	"品川",
+	"高輪ゲートウェイ",
+	"田町",
+	"浜松町",
+	"新橋",
+	"有楽町",
+];
---
+++ dev/app/src/pages/monitors/type313s/pages/correction/components/LocationCorrectionLineImage.tsx
@@ -0,0 +1,197 @@
+import { memo, useCallback } from "react";
+
+import { CanvasImage, CanvasText } from "../../../../../../canvas-renderer";
+import CanvasObjectBase from "../../../../../../canvas-renderer/objects/CanvasObjectBase";
+import { COLORS, DISPLAY_WIDTH } from "../../../constants";
+
+import type {
+	CanvasRenderFunction,
+	ClickEventHandler,
+} from "../../../../../../canvas-renderer/contexts/CanvasObjectContext";
+
+const WIDTH = DISPLAY_WIDTH;
+const HEIGHT = 70;
+
+const ROW_Y = 210;
+const STEP_Y = 104;
+
+export const CELL_COUNT = 4;
+
+const FIRST_CELL_LEFT = 64;
+const CELL_TOP = 34;
+const CELL_WIDTH = 104;
+const CELL_HEIGHT = HEIGHT - CELL_TOP;
+
+const STEP_X = 200;
+
+const LINE_TOP = 46;
+const LINE_HEIGHT = 10;
+const LINE_STRIPE_WIDTH = 32;
+const LINE_STRIPE_HEIGHT = LINE_HEIGHT - 2;
+const STROKE_THICKNESS = 1;
+
+type LocationCorrectionLineImageProps = {
+	row: number;
+	stationNameList: string[];
+	stationNameStartIndex: number;
+	carImageStationIndex: number;
+	onClick: (index: number) => void;
+};
+
+export default memo<LocationCorrectionLineImageProps>(
+	function LocationCorrectionLineImage({
+		row,
+		stationNameList,
+		stationNameStartIndex,
+		carImageStationIndex,
+		onClick,
+	}) {
+		const onRender: CanvasRenderFunction = useCallback(
+			(ctx, metadata) => {
+				ctx.save();
+
+				const ix = Math.round(metadata.absX);
+				const iy = Math.round(metadata.absY);
+				const iw = Math.round(metadata.width);
+
+				ctx.imageSmoothingEnabled = false;
+				ctx.fillStyle = COLORS.WHITE;
+				ctx.beginPath();
+				const lineAbsY = iy + LINE_TOP;
+				ctx.fillRect(ix, lineAbsY, iw, STROKE_THICKNESS);
+				ctx.fillRect(
+					ix,
+					lineAbsY + LINE_HEIGHT - STROKE_THICKNESS,
+					iw,
+					STROKE_THICKNESS
+				);
+				ctx.fillRect(ix, lineAbsY, STROKE_THICKNESS, LINE_HEIGHT);
+				ctx.fillRect(
+					ix + iw - STROKE_THICKNESS,
+					lineAbsY,
+					STROKE_THICKNESS,
+					LINE_HEIGHT
+				);
+
+				Array.from({ length: CELL_COUNT }).forEach((_, i) => {
+					const absX = ix + (FIRST_CELL_LEFT + STEP_X * i);
+					const absY = iy + CELL_TOP;
+					ctx.fillStyle = COLORS.BLACK;
+					ctx.fillRect(absX, absY, CELL_WIDTH, CELL_HEIGHT);
+					ctx.fillStyle = COLORS.WHITE;
+					ctx.fillRect(absX, absY, CELL_WIDTH, STROKE_THICKNESS);
+					ctx.fillRect(absX, absY + CELL_HEIGHT, CELL_WIDTH, STROKE_THICKNESS);
+					ctx.fillRect(absX, absY, STROKE_THICKNESS, CELL_HEIGHT);
+					ctx.fillRect(
+						absX + CELL_WIDTH - STROKE_THICKNESS,
+						absY,
+						STROKE_THICKNESS,
+						CELL_HEIGHT + STROKE_THICKNESS
+					);
+				});
+
+				ctx.fillStyle = COLORS.WHITE;
+				const stripeOffset0 = row % 2 === 0 ? LINE_STRIPE_WIDTH : 0;
+				const stripeOffset1 = row % 2 === 1 ? LINE_STRIPE_WIDTH : 0;
+				const cellEdge = FIRST_CELL_LEFT + CELL_WIDTH;
+				const stripes: number[] = [];
+
+				// 一番左
+				stripes.push(stripeOffset0);
+
+				// [0] - [1]
+				stripes.push(cellEdge + stripeOffset0);
+				if (stripeOffset0 === 0) {
+					stripes.push(cellEdge + LINE_STRIPE_WIDTH * 2);
+				}
+
+				// [1] - [2]
+				stripes.push(cellEdge + STEP_X + stripeOffset1);
+				if (stripeOffset1 === 0) {
+					stripes.push(cellEdge + STEP_X + LINE_STRIPE_WIDTH * 2);
+				}
+
+				// [2] - [3], 一番右
+				stripes.push(cellEdge + STEP_X * 2 + stripeOffset0);
+				if (stripeOffset0 === 0) {
+					stripes.push(cellEdge + STEP_X * 2 + LINE_STRIPE_WIDTH * 2);
+				} else {
+					// 一番右
+					stripes.push(cellEdge + STEP_X * 3);
+				}
+
+				stripes.forEach((sx) =>
+					ctx.fillRect(
+						ix + sx,
+						lineAbsY + 1,
+						LINE_STRIPE_WIDTH,
+						LINE_STRIPE_HEIGHT
+					)
+				);
+
+				ctx.restore();
+			},
+			[row]
+		);
+
+		const colCount = Math.min(
+			CELL_COUNT,
+			stationNameList.length - stationNameStartIndex
+		);
+		const handleClick: ClickEventHandler = useCallback(
+			(relX, relY) => {
+				if (relY < CELL_TOP || CELL_TOP + CELL_HEIGHT < relY) {
+					return false;
+				}
+				for (let i = 0; i < colCount; i++) {
+					const cellRelX = FIRST_CELL_LEFT + STEP_X * i;
+					if (cellRelX <= relX && relX <= cellRelX + CELL_WIDTH) {
+						const stationIndex = stationNameStartIndex + i;
+						onClick(stationIndex);
+						return true;
+					}
+				}
+				return false;
+			},
+			[colCount, onClick, stationNameStartIndex]
+		);
+
+		const carImageCol = carImageStationIndex - stationNameStartIndex;
+		const isCarImageVisible = 0 <= carImageCol && carImageCol < colCount;
+
+		return (
+			<CanvasObjectBase
+				onRender={onRender}
+				relX={0}
+				relY={ROW_Y + row * STEP_Y}
+				width={WIDTH}
+				height={HEIGHT}
+				onClick={handleClick}
+				isFilled>
+				{isCarImageVisible && (
+					<CanvasImage
+						imagePath="/type313s/location_correction_car.png"
+						relX={FIRST_CELL_LEFT + STEP_X * carImageCol}
+						relY={0}
+						areaWidth={CELL_WIDTH}
+						horizontalAlign="center"
+					/>
+				)}
+				{Array.from({ length: colCount }).map((_, i) => (
+					<CanvasText
+						// eslint-disable-next-line react/no-array-index-key
+						key={`station-name-${i}`}
+						text={stationNameList[i + stationNameStartIndex] ?? ""}
+						relX={FIRST_CELL_LEFT + STEP_X * i}
+						relY={CELL_TOP}
+						maxWidthPx={CELL_WIDTH}
+						maxHeightPx={CELL_HEIGHT}
+						align="center"
+						verticalAlign="center"
+						fillColor={COLORS.WHITE}
+					/>
+				))}
+			</CanvasObjectBase>
+		);
+	}
+);
