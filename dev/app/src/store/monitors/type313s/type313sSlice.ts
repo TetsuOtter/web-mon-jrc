70ea8dc7af0627429f3f81b3d236697920833f9e
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2026-03-01 15:40:24 +0900

    車両状態表示用の型を追加

--- dev/app/src/store/monitors/type313s/type313sSlice.ts
+++ dev/app/src/store/monitors/type313s/type313sSlice.ts
@@ -1,17 +1,198 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import type { Type313sState } from "./type313sTypes";
 import type { PayloadAction } from "@reduxjs/toolkit";
-
-export type Type313sState = {
-	trainNumber?: string;
-	trainType?: string;
-	destination?: string;
-	currentLocation: number;
-	timeMs?: number;
-};
 
 const initialState: Type313sState = {
 	currentLocation: 123.4,
+
+	carStateList: [
+		{
+			carType: "Mc",
+			carNumber: 1,
+			hasLeftPantograph: false,
+			hasRightPantograph: true,
+
+			isDoorClosed: true,
+			isAnnounceOn: true,
+			airConditionerState: "AUTO_COOLING",
+			fanState: "AUTO_HIGH",
+			temperature: 25,
+			humidity: 50,
+			coolingTargetTemperature: 22,
+			heatingTargetTemperature: 28,
+			airConditionerAdjustmentMode: "NORMAL",
+			isVentOn: false,
+			isRoomLightOn: true,
+			occupancy: 100,
+			occupancyRate: 0.5,
+
+			carStates: {
+				isMSOn: true,
+				isCPOn: undefined,
+				isMRPressureNormal: true,
+				isTestSWOn: false,
+				isSnowBrakeOn: false,
+
+				sivLineState: {
+					isCgKForSIVOn: true,
+					isIvMSOn: true,
+					isIvHBOn: true,
+					isIvLOn: true,
+					isSIVOn: true,
+					is3phMKOn: true,
+					isIvCNOn: true,
+					isIVSMDSOn: true,
+					sivVoltage: 442,
+					sivFrequency: 60,
+				},
+				isReduceLoadOn: false,
+
+				bcPressure: [300, 300, 300, 300],
+				mrPressure: 860,
+				receivedNotchCommand: "非常",
+			},
+
+			cabState: {
+				side: "left",
+				cabSesState: "FORWARD",
+				isBHEBOn: false,
+				isConductorEBOn: false,
+				isSpareStraightBrakeOn: false,
+				orderedNotchCommand: "非常",
+				isTLKOn: false,
+			},
+
+			bogieState: {
+				isHBOn: true,
+				isCCOSNormal: true,
+				isLB1On: true,
+				vvvf2State: "VVVF",
+				isCgKForVVVF2On: true,
+				lineVoltage: 1650,
+
+				left: {
+					isLbOn: true,
+					mmCurrent1: 0,
+					mmCurrent2: 0,
+					isMCOS1On: true,
+					isMCOS2On: true,
+				},
+				right: {
+					isLbOn: true,
+					mmCurrent1: 0,
+					mmCurrent2: 0,
+					isMCOS1On: true,
+					isMCOS2On: true,
+				},
+			},
+		},
+		{
+			carType: "M",
+			carNumber: 2,
+			hasLeftPantograph: false,
+			hasRightPantograph: false,
+
+			isDoorClosed: true,
+			isAnnounceOn: true,
+			airConditionerState: "AUTO_COOLING",
+			fanState: "AUTO_HIGH",
+			temperature: 25,
+			humidity: 50,
+			coolingTargetTemperature: 22,
+			heatingTargetTemperature: 28,
+			airConditionerAdjustmentMode: "NORMAL",
+			isVentOn: false,
+			isRoomLightOn: true,
+			occupancy: 100,
+			occupancyRate: 0.5,
+
+			carStates: {
+				isMSOn: true,
+				isCPOn: undefined,
+				isMRPressureNormal: true,
+				isTestSWOn: false,
+				isSnowBrakeOn: false,
+
+				sivLineState: undefined,
+				isReduceLoadOn: false,
+
+				bcPressure: [300, 300, 300, 300],
+				mrPressure: 860,
+				receivedNotchCommand: "非常",
+			},
+
+			cabState: undefined,
+
+			bogieState: {
+				isHBOn: true,
+				isCCOSNormal: true,
+				isLB1On: true,
+				vvvf2State: "VVVF",
+				isCgKForVVVF2On: true,
+				lineVoltage: 1650,
+
+				left: {
+					isLbOn: true,
+					mmCurrent1: 0,
+					mmCurrent2: 0,
+					isMCOS1On: true,
+					isMCOS2On: true,
+				},
+				right: undefined,
+			},
+		},
+		{
+			carType: "Tc",
+			carNumber: 3,
+			hasLeftPantograph: false,
+			hasRightPantograph: false,
+
+			isDoorClosed: true,
+			isAnnounceOn: true,
+			airConditionerState: "AUTO_COOLING",
+			fanState: "AUTO_HIGH",
+			temperature: 25,
+			humidity: 50,
+			coolingTargetTemperature: 22,
+			heatingTargetTemperature: 28,
+			airConditionerAdjustmentMode: "NORMAL",
+			isVentOn: false,
+			isRoomLightOn: true,
+			occupancy: 100,
+			occupancyRate: 0.5,
+
+			carStates: {
+				isMSOn: true,
+				isCPOn: undefined,
+				isMRPressureNormal: true,
+				isTestSWOn: false,
+				isSnowBrakeOn: false,
+
+				sivLineState: undefined,
+				isReduceLoadOn: false,
+
+				bcPressure: [300, 300, 300, 300],
+				mrPressure: 860,
+				receivedNotchCommand: "非常",
+			},
+
+			cabState: {
+				side: "right",
+				cabSesState: "REVERSE",
+				isBHEBOn: false,
+				isConductorEBOn: false,
+				isSpareStraightBrakeOn: false,
+				isTLKOn: false,
+			},
+
+			bogieState: undefined,
+		},
+	],
+	conductorState: {
+		isRoomLightOn: true,
+		isGuidanceOn: true,
+	},
 };
 
 const type313sSlice = createSlice({
---
+++ dev/app/src/store/monitors/type313s/type313sTypes.ts
@@ -0,0 +1,137 @@
+export type Type313sState = {
+	trainNumber?: string;
+	trainType?: string;
+	destination?: string;
+	currentLocation: number;
+	timeMs?: number;
+
+	carStateList: Type313sCarState[];
+	conductorState: Type313sConductorState;
+};
+
+export type Type313sConductorState = {
+	isRoomLightOn: boolean | null;
+	isGuidanceOn: boolean | null;
+};
+
+export type Type313sCarState = {
+	// 編成表示系
+	carType: string;
+	carNumber: number;
+	hasLeftPantograph: boolean;
+	hasRightPantograph: boolean;
+
+	// 車掌系
+	isDoorClosed: boolean | null;
+	isAnnounceOn: boolean | null;
+	airConditionerState: AirConditionerState | null;
+	fanState: FanState | null;
+	temperature: number | null;
+	humidity: number | null;
+	coolingTargetTemperature: number | null;
+	heatingTargetTemperature: number | null;
+	airConditionerAdjustmentMode: AirConditionerAdjustmentMode | null;
+	isVentOn: boolean | null;
+	isRoomLightOn: boolean | null;
+
+	// 乗車率
+	occupancy: number | null;
+	occupancyRate: number | null;
+
+	// 車両状態
+	carStates: {
+		isMSOn?: boolean | null;
+		isCPOn?: boolean | null;
+		isMRPressureNormal: boolean | null;
+		isTestSWOn: boolean | null;
+		isSnowBrakeOn: boolean | null;
+
+		sivLineState?: {
+			isCgKForSIVOn: boolean | null;
+			isIvMSOn: boolean;
+			isIvHBOn: boolean;
+			isIvLOn: boolean;
+			isSIVOn: boolean | null;
+			is3phMKOn: boolean | null;
+			isIvCNOn: boolean | null;
+			isIVSMDSOn: boolean | null;
+			sivVoltage: number | null;
+			sivFrequency: number | null;
+		};
+		isReduceLoadOn: boolean | null;
+
+		bcPressure: [number | null, number | null, number | null, number | null];
+		mrPressure: number | null;
+		receivedNotchCommand: string | null;
+	};
+
+	cabState?: Type313sCabState;
+	bogieState?: Type313sBogieCommonState;
+};
+
+export type Type313sCabState = {
+	side: "left" | "right";
+	cabSesState: CabSesState | null;
+	isBHEBOn: boolean | null;
+	isConductorEBOn: boolean | null;
+	isSpareStraightBrakeOn: boolean | null;
+	orderedNotchCommand?: string | null;
+	isTLKOn: boolean | null;
+};
+export type Type313sBogieCommonState = {
+	isHBOn: boolean | null;
+	isCCOSNormal: boolean | null;
+	isLB1On: boolean | null;
+	vvvf2State: VVVF2State | null;
+	isCgKForVVVF2On: boolean | null;
+	lineVoltage: number | null;
+
+	left?: Type313sBogieState;
+	right?: Type313sBogieState;
+};
+export type Type313sBogieState = {
+	isLbOn: boolean | null;
+	mmCurrent1: number | null;
+	mmCurrent2: number | null;
+	isMCOS1On: boolean | null;
+	isMCOS2On: boolean | null;
+};
+
+export const AIR_CONDITIONER_STATE = {
+	AUTO_HEATING: "AUTO_HEATING",
+	AUTO_COOLING: "AUTO_COOLING",
+	OFF: "OFF",
+} as const;
+export type AirConditionerState =
+	(typeof AIR_CONDITIONER_STATE)[keyof typeof AIR_CONDITIONER_STATE];
+
+export const FAN_STATE = {
+	AUTO_HIGH: "AUTO_HIGH",
+	AUTO_LOW: "AUTO_LOW",
+	AUTO_OFF: "AUTO_OFF",
+	MANUAL_HIGH: "MANUAL_HIGH",
+	MANUAL_LOW: "MANUAL_LOW",
+	MANUAL_OFF: "MANUAL_OFF",
+} as const;
+export type FanState = (typeof FAN_STATE)[keyof typeof FAN_STATE];
+
+export const AIR_CONDITIONER_ADJUSTMENT_MODE = {
+	HIGH: "HIGH",
+	NORMAL: "NORMAL",
+	LOW: "LOW",
+} as const;
+export type AirConditionerAdjustmentMode =
+	(typeof AIR_CONDITIONER_ADJUSTMENT_MODE)[keyof typeof AIR_CONDITIONER_ADJUSTMENT_MODE];
+
+export const CAB_SES_STATE = {
+	FORWARD: "FORWARD",
+	NEUTRAL: "NEUTRAL",
+	REVERSE: "REVERSE",
+} as const;
+export type CabSesState = (typeof CAB_SES_STATE)[keyof typeof CAB_SES_STATE];
+
+export const VVVF2_STATE = {
+	VVVF: "VVVF",
+	VVVF2: "VVVF2",
+} as const;
+export type VVVF2State = (typeof VVVF2_STATE)[keyof typeof VVVF2_STATE];
