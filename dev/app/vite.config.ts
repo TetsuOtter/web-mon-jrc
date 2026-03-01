83bd3e745b54406d7a91cf78b86243ca35cd4c5a
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2026-03-01 23:38:39 +0900

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- dev/app/vite.config.ts
+++ dev/app/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+	base: process.env.VITE_BASE || "/",
 	plugins: [react()],
 	server: {
 		allowedHosts: ["localhost", "127.0.0.1"],
