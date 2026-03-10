c0df9b0bdd9d8bb73140eaf8f7a8dc96a6143c27
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2026-03-10 22:34:31 +0900

    Update Type 313S link to include PAGE_TYPES_MENU constant for improved readability

--- dev/app/src/pages/index/IndexPage.tsx
+++ dev/app/src/pages/index/IndexPage.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 import { ROUTES } from "../../router-paths";
 
+const PAGE_TYPES_MENU = "MENU";
+
 export default memo(function IndexPage() {
 	return (
 		<div style={{ padding: "20px" }}>
@@ -13,7 +15,7 @@ export default memo(function IndexPage() {
 						<Link to={ROUTES.SETTINGS.path}>Settings</Link>
 					</li>
 					<li>
-						<Link to={ROUTES.TYPE313S.path}>Type 313S</Link>
+						<Link to={`/monitors/type313s/${PAGE_TYPES_MENU}`}>Type 313S</Link>
 					</li>
 					<li>
 						<Link to={ROUTES.TYPE313S_EDIT.path}>Type 313S Edit</Link>
